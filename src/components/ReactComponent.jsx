6bc384d563b60ec9feed147274b8474e0ed5f791
Jonathan Sun <Jspsun@gmail.com>
2019-12-25 18:53:35 -0500

    strip out non sandbox code

--- src/components/ReactComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class ReactComponent extends Component {
-  render() {
-    return (
-      <div>This is a react component</div>
-    );
-  }
-}
